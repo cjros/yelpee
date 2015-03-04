0a8d268b2d946059003b63785b8e3042734ce76e
maprules1000 <maprules1000@gmail.com>
2015-03-03 22:20:35 -0600

    printing out coffee shops

--- js/coffeeproject.js
+++ js/coffeeproject.js
@@ -1,120 +1,134 @@
-;(function(exports){
-	'use strict';
+;
+(function(exports) {
+    'use strict';
 
-	var collectionData = [{
-		name: "Catalina",
-		address: "123 Fake St",
-		photo_url: "http://i.imgur.com/z2eifSm.jpg",
-		rating: 10
-	},{
-		name: "Tout Suite",
-		address: "123 Fake St",
-		photo_url: "http://i.imgur.com/z2eifSm.jpg",
-		raiting: 10
-	}];
+    var collectionData = [{
+        name: "Catalina",
+        address: "123 Fake St",
+        photo_url: "http://i.imgur.com/z2eifSm.jpg",
+        rating: 10
+    }, {
+        name: "Tout Suite",
+        address: "123 Fake St",
+        photo_url: "http://i.imgur.com/z2eifSm.jpg",
+        raiting: 10
+    }];
 
-	Backbone.CSRouter = Backbone.Router.extend({
-		initialize: function(){
-			this.container = document.querySelector('.container');
-			this.collection = new Backbone.CoffeeShopList();
+    Backbone.CSRouter = Backbone.Router.extend({
+        initialize: function() {
+            this.container = document.querySelector('.container');
+            this.collection = new Backbone.CoffeeShopList();
 
-			//incorporating collection
-			// this.firstPage = z(Backbone.LandingView, {collection: this.collection});
+            this.homePage = z(Backbone.LandingView, {
+                collection: this.collection
+            });
 
-			Backbone.history.start();
-		},
-		routes: {
-			'*default': 'home'
-			// '/login': 'login'
-		},
-		home: function(){
-			var self = this;
-			console.log('hi');
-			// this.model = new Backbone.CoffeeShop(); // the model
-			// this.firstPage = z(Backbone.LandingView, {model: this.model}); //might need to put into a promise if errors in the future
+            //incorporating collection
+            // this.firstPage = z(Backbone.LandingView, {collection: this.collection});
 
-			this.collection.fetch().then(function(data){
-				console.log(data)
-				// React.render(self.firstPage, self.container);
-			})
-		},
-		login: function(){
-		// 	this.loginPage = z(Backbone.LoginView, {model: this.props.model})
-		// 	React.render(this.loginPage, this.container);
-		}
-	});
+            Backbone.history.start();
+        },
+        routes: {
+            '*default': 'home'
+                // '/login': 'login'
+        },
+        home: function() {
+            var self = this;
+            console.log('hi');
+            // this.model = new Backbone.CoffeeShop(); // the model
+            // this.firstPage = z(Backbone.LandingView, {model: this.model}); //might need to put into a promise if errors in the future
 
-	Backbone.CoffeeShop = Backbone.Model.extend({
-		url: function(){
-			return "http://coffeesnob-api.herokuapp.com/api/shops/"
-		},
-		defaults: {
-			name: "no name"
-		}
-	})
+            this.collection.fetch().then(function(data) {
+                console.log(data)
+                React.render(self.homePage, self.container);
+            })
+        },
+        login: function() {
+            // 	this.loginPage = z(Backbone.LoginView, {model: this.props.model})
+            // 	React.render(this.loginPage, this.container);
+        }
+    });
 
-	Backbone.CoffeeShopComments = Backbone.Model.extend({
-		url: function(){
-			return "http://coffeesnob-api.herokuapp.com/api/shops/"+this.get('shop_id')
-		},
-		defaults: {
-			shop_id: 1,
-			message: "No Comment."
-		}
-	})
+    Backbone.CoffeeShop = Backbone.Model.extend({
+        url: function() {
+            return "http://coffeesnob-api.herokuapp.com/api/shops/"
+        },
+        defaults: {
+            name: "no name"
+        }
+    })
 
-	Backbone.CoffeeShopList = Backbone.Collection.extend({
+    Backbone.CoffeeShopComments = Backbone.Model.extend({
+        url: function() {
+            return "http://coffeesnob-api.herokuapp.com/api/shops/" + this.get('shop_id')
+        },
+        defaults: {
+            shop_id: 1,
+            message: "No Comment."
+        }
+    })
+
+    Backbone.CoffeeShopList = Backbone.Collection.extend({
         model: Backbone.CoffeeShop,
         url: 'http://coffeesnob-api.herokuapp.com/api/shops'
     })
 
-	var Mapify = React.createClass({
-		render: function(){
-			return z()
-		}
-	})
+    var Mapify = React.createClass({
+        render: function() {
+            return z()
+        }
+    })
 
-	Backbone.LandingView = React.createClass({
-		render: function(){
-			var model = this.props.model;
-			var arr = ['hi', 'hello', 'sup', 'bye']
-			// debugger;
-			console.log(model)
-			return z('div.wrapper', [
-						z('div.header', [
-							z('div.nav', [
-								z('ul', [
-									z('li#snob', 'coffeeSnob'),
-									z('li#home', 'Home'),
-									z('li#about', 'About'),
-									z('li#sign-in', 'Sign In'),
-									z('li#register', 'Register')
-								])
-							])
-						]),
-						z('div.map', model.get('shops')[0].id),
-						arr.map(function(model){
-							return z('div#'+model, model)
-						})
-					])
-		}
-	});
+    Backbone.LandingView = React.createClass({
+        componentWillMount: function() { //what happens when the object is attached to the dom similar to initialize
+            var self = this;
+            this.props.collection && this.props.collection.on("change reset add remove", function() {
+                self.forceUpdate() //setup listener and then force update on collection change
+            })
+        },
+        render: function() {
+            var model = this.props.model;
+            console.log(model)
+            return z('div.wrapper', [
+                z('div.header', [
+                    z('div.nav', [
+                        z('ul', [
+                            z('li#snob', 'coffeeSnob'),
+                            z('li#home', 'Home'),
+                            z('li#about', 'About'),
+                            z('li#sign-in', 'Sign In'),
+                            z('li#register', 'Register')
+                        ])
+                    ])
+                ]),
+                // z('div.map', model.get('shops')[0].id),
+                z('ol',
+                    this.props.collection.models[0].attributes.shops.map(function(i) {
+                    	console.log(i.name);
+                        return z('li#' + i.id, i.name);
+                    })
+                )
+            ])
+        }
+    });
 
-	Backbone.LoginView = React.createClass({
-		_test: function() {
-			console.log(arguments)
-		},
-		render: function(){
-			return z('div.login-wrapper', [
-				z('div.form-wrapper', [
-					z('form.login-info', [
-						z('input:text@username'),
-						z('input:password@password'),
-						z('button', {onSubmit: this._test},'LOGIN')
-					])
-				])
-			])
-		}
-	})
-	
-})(typeof module === 'object' ? module.exports: window)
+    Backbone.LoginView = React.createClass({
+        _test: function() {
+            console.log(arguments)
+        },
+        render: function() {
+            return z('div.login-wrapper', [
+                z('div.form-wrapper', [
+                    z('form.login-info', [
+                        z('input:text@username'),
+                        z('input:password@password'),
+                        z('button', {
+                            onSubmit: this._test
+                        }, 'LOGIN')
+                    ])
+                ])
+            ])
+        }
+    })
+
+})(typeof module === 'object' ? module.exports : window)
