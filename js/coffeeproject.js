2780770f2468b85eb59e4c54cf2295d015a6f13d
maprules1000 <maprules1000@gmail.com>
2015-03-08 18:55:30 -0500

    formatting details

--- js/coffeeproject.js
+++ js/coffeeproject.js
@@ -2,12 +2,8 @@
 (function(exports) {
     'use strict';
 
-    Math.clamp = function(value, floor, ceil){
-    	return Math.min(ceil, Math.max(floor, value))
-    }
-
-    var webify = function(string){
-    	return string.replace(/ /g, "+")
+    Math.clamp = function(value, floor, ceil) {
+        return Math.min(ceil, Math.max(floor, value));
     }
 
     Backbone.CSRouter = Backbone.Router.extend({
@@ -17,9 +13,9 @@
             Backbone.history.start();
         },
         routes: {
-        	'home': 'home',
-        	'about': 'about',
-        	'contact': 'contact',
+            'home': 'home',
+            'about': 'about',
+            'contact': 'contact',
             ':shop_id': 'shopper',
             '*default': 'home'
         },
@@ -36,30 +32,37 @@
             })
         },
         about: function() {
-        	console.log("Rendering About Us");
+            console.log("Rendering About Us");
         },
         contact: function() {
-        	console.log("Rendering Contact Page");
+            console.log("Rendering Contact Page");
         },
-        shopper: function(shop_id){
+        shopper: function(shop_id) {
             var self = this;
             this.commentsCollection = new Backbone.CoffeeShopCommentList();
             this.commentsCollection.shop_id = shop_id;
-            this.detailPage = z(Backbone.DetailView, {collection: this.commentsCollection})
 
-            this.commentsCollection.fetch().then(function(data){
-                // debugger;
-                React.render(self.detailPage, self.container)
+            this.shopInfo = new Backbone.CoffeeShop();
+            this.shopInfo.shop_id = shop_id;
+
+            this.detailPage = z(Backbone.DetailView, {
+                collection: this.commentsCollection,
+                shop: this.shopInfo
+            });
+
+            $.when(this.commentsCollection.fetch(), this.shopInfo.fetch()).then(function() {
+                React.render(self.detailPage, self.container);
             })
         }
     });
 
     Backbone.CoffeeShop = Backbone.Model.extend({
-        // url: function() {
-        //     return "http://coffeesnob-api.herokuapp.com/api/shops/" //+ 1
-        // }
-    })
-    /* shops collection */
+            url: function() {
+                // debugger;
+                return "http://coffeesnob-api.herokuapp.com/api/shops/" + this.shop_id
+            }
+        })
+        /* shops collection */
     Backbone.CoffeeShopList = Backbone.Collection.extend({
         model: Backbone.CoffeeShop,
         url: function() {
@@ -69,7 +72,7 @@
 
     Backbone.CoffeeShopComment = Backbone.Model.extend({
         url: function() {
-            return ' http://coffeesnob-api.herokuapp.com/api/shops/'+this.collection.shop_id+'/comments/'
+            return ' http://coffeesnob-api.herokuapp.com/api/shops/' + this.collection.shop_id + '/comments/'
         }
     });
     /* comments collection */
@@ -77,45 +80,40 @@
         model: Backbone.CoffeeShopComment,
         url: function() {
             // debugger;
-            return 'http://coffeesnob-api.herokuapp.com/api/shops/'+this.shop_id+'/comments/'
+            return 'http://coffeesnob-api.herokuapp.com/api/shops/' + this.shop_id + '/comments/'
         },
-        parse: function(data){
+        parse: function(data) {
             // debugger;
             return data.comments;
         }
     });
 
     Backbone.CoffeeShopVote = Backbone.Model.extend({
-    	url: function() {
-    		return 'http://coffeesnob-api.herokuapp.com/api/shops/'+this.get('shop_id')+'/votes';
-    	}
+        url: function() {
+            return 'http://coffeesnob-api.herokuapp.com/api/shops/' + this.get('shop_id') + '/votes';
+        }
     });
 
     Backbone.HeaderComponent = React.createClass({
-		displayName: 'HeaderComponent',
-		render: function() {
-			return z('div.header', [
-				z('div.nav', [
-					z('ul', [
-						z('a[href=#home]', [
-							z('li.home', 'CoffeeSnob')]),
-						z('a[href=#about]', [
-							z('li.about', 'About')]),
-						z('a[href=#contact]', [
-							z('li.contact', 'Contact')
-						])
-					])
-				])
-			])
-		}
-	});
-
-	Backbone.ShopCard = React.createClass({
-		displayName: 'ShopCard',
-		render: function() {
-			return ""
-		}
-	});
+        displayName: 'HeaderComponent',
+        render: function() {
+            return z('div.header', [
+                z('div.nav', [
+                    z('ul', [
+                        z('a[href=#home]', [
+                            z('li.home', 'CoffeeSnob')
+                        ]),
+                        z('a[href=#about]', [
+                            z('li.about', 'About')
+                        ]),
+                        z('a[href=#contact]', [
+                            z('li.contact', 'Contact')
+                        ])
+                    ])
+                ])
+            ])
+        }
+    });
 
     Backbone.LandingView = React.createClass({
         getInitialState: function() {
@@ -139,43 +137,51 @@
             console.log(this.props.collection)
             return z('div.wrapper', [
                 z(Backbone.HeaderComponent),
-                // z('div.map', model.get('shops')[0].id),
                 z('div.main', [
                     z('div.spacer', ''),
                     this.props.collection.models[0].attributes.shops.map(function(i) {
-                    	// debugger;
-                    	var stars = new Array(Math.clamp(i.rating, 0, 5)).join(',').split(',').map(function(v, i){
-	                    		return z('i.fa.fa-coffee', {key: Math.random()})
-	                    	}),
-                    		nonstars = (5 - stars.length) ? 
-                    			new Array(5 - stars.length).join(',').split(',').map(function(v, i){
-		                    		return z('i.fa.fa-coffee.empty-star', {key: Math.random()})
-		                    	}) : [],
-		                    rating = stars.concat(nonstars)
+                        var stars = new Array(Math.clamp(i.average_rating, 0, 5)).join(',').split(',').map(function(v, i) {
+                                return z('i.fa.fa-coffee', {
+                                    key: Math.random()
+                                })
+                            }),
+                            nonstars = (5 - stars.length) ?
+                            new Array(5 - stars.length).join(',').split(',').map(function(v, i) {
+                                return z('i.fa.fa-coffee.empty-star', {
+                                    key: Math.random()
+                                })
+                            }) : [],
+                            rating = stars.concat(nonstars)
+
+
 
                         return z('div#' + i.id + ".shopCard", [
                             z('div#image' + i.id + ".imgBox", [
-                                z('a[href=#'+i.id+']', [z('img.' + i.id + "[src="+i.photo.photo.medium.url+"]")])
+                                z('a[href=#' + i.id + ']', [z('img.' + i.id + "[src=" + i.photo.photo.medium.url + "]")])
                             ]),
                             z('div#' + i.id + ".shopDeets", [
                                 z('div.shopInfo', [
-                                    z('a[href=#'+i.id+']', [z('div.shop_name', i.name)]),
-                                    z('br', {key: 1}),
+                                    z('a[href=#' + i.id + ']', [z('div.shop_name', i.name)]),
+                                    z('br', {
+                                        key: 1
+                                    }),
                                     z('div.rating', [
                                         z('span.stars', rating)
                                         //z('i.fa.fa-star', ")"), //will need to draw start based on rating
                                     ]),
-                                    z('br', {key: 2}),
-                                    z('a.website'+'[href='+i.website+']', i.website)
+                                    z('br', {
+                                        key: 2
+                                    }),
+                                    z('a.website' + '[href=' + i.website + ']', i.website)
                                 ]),
                                 z('div.shopContact', [
-                                    z('address', [i.address, z('br'), i.city+" "+i.state+" "+i.zip]),
+                                    z('address', [i.address, z('br'), i.city + " " + i.state + " " + i.zip]),
                                     z('span.phoneNo', i.phone)
-                                ])   
+                                ])
                             ]),
-                            z('div.shopSummary',[
-                                	z('i.fa.fa-comments.fa-lg'),
-                                	z('div.summaryText',i.description)
+                            z('div.shopSummary', [
+                                z('i.fa.fa-comments.fa-lg'),
+                                z('div.summaryText', i.description)
                             ])
                         ])
                     })
@@ -202,7 +208,7 @@
         componentWillUnmount: function() {
             this.props.collection && this.props.collection.off("change reset add remove")
         },
-        _addComment: function(e){
+        _addComment: function(e) {
             e.preventDefault();
             // debugger;
             console.log(this.props)
@@ -213,56 +219,81 @@
 
             var self = this;
             this.props.collection.create(data)
-        
 
-
-            // var newComment = new Backbone.CoffeeShopComment(data)
-
-            // var self = this;
-            // newComment.save().then(function(){
-            //     //WHY ISNT THIS SHIT RE-RENDERING?
-            //     //comment actually gets added but dev tools shows an error
-            //     //must manually refresh for the comment to even show
-
-            //     //EDIT: SHIT ISNT EVEN POSTING ANYMORE
-            //     self.props.collection.fetch()
-            // })
         },
         render: function() {
-        	var mapAddress = "https://maps.googleapis.com/maps/api/staticmap?center=2201+Washington+Ave,Houston+TX&zoom=16&size=400x400&markers=2201+Washington+Ave,Houston+TX";
-        	var minMapUrl = "https://maps.googleapis.com/maps/api/staticmap?center=2201+Washongton+Ave,Houston+TX&zoom=16&size=400x400";
-        	var testURL = "http://i.imgur.com/kCrDFvC.jpg";
-        	var x = "https://maps.googleapis.com/maps/api/staticmap?center=San+Francisco,CA&zoom=10&size=400x400";
+
+            var shop = this.props.shop.get('shop');
+            var stars = new Array(Math.clamp(shop.average_rating, 0, 5)).join(',').split(',').map(function(v, i) {
+                    return z('i.fa.fa-coffee', {
+                        key: Math.random()
+                    })
+                }),
+                nonstars = (5 - stars.length) ? new Array(5 - stars.length).join(',').split(',').map(function(v, i) {
+                    return z('i.fa.fa-coffee.empty-star', {
+                        key: Math.random()
+                    })
+                }) : [],
+                rating = stars.concat(nonstars)
+
+            //props has collection of models at this point
+            //HOW DO SHOP DETAILS GET PASSED INTO HERE?
             return z('div.wrapper', [
-            	z(Backbone.HeaderComponent),
+                z(Backbone.HeaderComponent),
 
                 z('div.main', [
-                	z('div.spacer', ''),
-                	z('div.details-box', [
-                		z('div.shop-details', 'INFO goes here'),
-                    	z('div.map', [
-                    		z('img[src='+']') //DOES NOT LIKE EXTRA EQUAL SIGNS
-                    	]),
-                	]),
+                    z('div.spacer', ''),
+                    z('div.details-box', [
+                        z('div.shopCard', [
+                            z('div#image' + shop.id + ".imgBox", [
+                                z('img.' + shop.id + "[src=" + shop.photo.photo.medium.url + "]")
+                            ]),
+                            z('div#' + shop.id + ".shopDeets", [
+                                z('div.shopInfo', [
+                                    z('a', [z('div.shop_name', shop.name)]),
+                                    z('br', {
+                                        key: 1
+                                    }),
+                                    z('div.rating', [
+                                        z('span.stars', rating)
+                                        //z('i.fa.fa-star', ")"), //will need to draw start based on rating
+                                    ]),
+                                    z('br', {
+                                        key: 2
+                                    }),
+                                    z('a.website' + '[href=' + shop.website + ']', shop.website)
+                                ]),
+                                z('div.shopContact', [
+                                	z('img'),
+                                    z('address', [shop.address, z('br'), shop.city + " " + shop.state + " " + shop.zip]),
+                                    z('span.phoneNo', shop.phone)
+                                ])
+                            ]),
+                            z('div.shopSummary', [
+                                z('i.fa.fa-comments.fa-lg'),
+                                z('div.summaryText', shop.description)
+                            ])
+
+                        ]),
+                    ]),
+
                     z('form.comment-box', {
                         onSubmit: this._addComment
                     }, [
-                        z('input:text@addComment[placeholder=add a new comment]'),
-                        z('button', 'submit!')
-                    ]),
-                    z('ol', [
-                        this.props.collection.map(function(i) {
-                            return z('li#' + i.id, [
-                                z('div.info-container', [
-                                    z('div.shop_name', i.get('message'))
+                        z('textarea.userComment@addComment[placeholder=add a new comment]'),
+                        z('button', 'submit!'),
+                        z('ol', [
+                            this.props.collection.map(function(i) {
+                                return z('li#' + i.id, [
+                                    z('div.info-container', [
+                                        z('div.shop_name', i.get('message'))
+                                    ])
                                 ])
-                            ])
-
-                        })
+                            })
+                        ])
                     ])
                 ])
             ])
-
         }
     });
 
