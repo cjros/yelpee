1da4f4a9db5e3c6f488e46793c43c72664b2a829
cjros <clor09@gmail.com>
2015-03-08 15:28:17 -0500

    updated backup coffeetesting code

--- js/coffeetesting.js
+++ js/coffeetesting.js
@@ -9,6 +9,7 @@
 			Backbone.history.start()
 		},
 		routes: {
+			':id': 'shop',
 			'*default': 'home'
 		},
 		home: function() {
@@ -16,18 +17,33 @@
 			var self = this;
 
 			this.collection = new Backbone.CoffeeShopCollection();
-			this.homeView = z(Backbone.HomeView, {collection: this.collection});
+			this.view = z(Backbone.HomeView, {collection: this.collection});
 			this.collection.fetch().then(function(d) {
-				// console.log(d)
-				// console.log(self.collection)
-				React.render(self.homeView, self.container);
+				React.render(self.view, self.container);
 			});
+		},
+		shop: function(id) {
+			var self = this;
+			this.collection = new Backbone.CommentListCollection();
+			this.collection.id = id;
+			this.view = z(Backbone.CommentsView, {collection: this.collection});
+			this.collection.fetch().then(function(d) {
+				console.log(self.collection)
+				console.log(d)
+				React.render(self.view, self.container)
+			})
 		}
 	});
 
 	Backbone.CoffeeShopModel = Backbone.Model.extend({
 		// url: function() {
 		// 	return 'http://coffeesnob-api.herokuapp.com/api/shops'  //shop.id
+		// }
+	});
+
+	Backbone.CommentModel = Backbone.Model.extend({
+  //       url: function() {
+		// 	return 'http://coffeesnob-api.herokuapp.com/api/shops/' + this.collection.id + '/comments'
 		// }
 	});
 
@@ -41,13 +57,23 @@
 		}
 	});
 
+	Backbone.CommentListCollection = Backbone.Collection.extend({
+		model: Backbone.CommentModel,
+		url: function() {
+			return 'http://coffeesnob-api.herokuapp.com/api/shops/' + this.id + '/comments'
+		},
+		parse: function(data) {
+			return data.comments
+		}
+	});
+
 	Backbone.HeaderComponent = React.createClass({
 		displayName: 'HeaderComponent',
 		render: function() {
 			return z('div.HeaderComponent', [
 				z('div.nav', [
 					z('ul', [
-						z('a[href=#home]', [
+						z('a[href=#]', [
 							z('li.home', 'Home')]),
 						z('a[href=#about]', [
 							z('li.about', 'About')]),
@@ -60,30 +86,77 @@
 		}
 	});
 
-	Backbone.ShopCollectionComponent = React.createClass({
-		displayName: 'ShopCollectionComponent',
+	Backbone.ShopComponent = React.createClass({
+		displayName: 'ShopComponent',
 		render: function() {
-			var aShop = this.props.models;
-			return z('div.ShopColletionComponent', [
-				aShop.map(function(d) {
-					console.log(d.get('name'))
-				})
-			])
+			var aShop = this.props.data;
+			return z('a[href=#'+aShop.get('id')+']', aShop.get('name'))
+		}
+	});
+
+	Backbone.CommentComponent = React.createClass({
+		displayName: 'CommentComponent',
+		render: function() {
+			var aComment = this.props.d;
+			return z('div.comment'+aComment.get('id'), aComment.get('message'))
 		}
 	});
 
 	Backbone.HomeView = React.createClass({
+		displayName: 'HomeView',
 		render: function() {
-			console.log(this)
-			return z('div.wrapper', [
+			var shops = this.props.collection.models;
+			console.log(shops);
+			return z('div.homeview-wrapper', [
 				z(Backbone.HeaderComponent),
-				z(Backbone.ShopCollectionComponent, {models: this.props.collection.models})
+ 				z('div.ShopsCollectionComponent', 
+ 					shops.map(function(data, index, arr) {
+ 						console.log(data);
+ 						return z(Backbone.ShopComponent, {key: index, data: data});
+ 					})
+ 				)
+				// z(Backbone.ShopsCollectionComponent, {shops: this.props.collection.models})
 			])
 		}
 	});
 
-	
-
+	Backbone.CommentsView = React.createClass({
+		displayName: 'CommentsView',
+		componentWillMount: function() {
+            var self = this
+            this.props.collection && this.props.collection.on("change reset add remove", function(){
+                self.forceUpdate()
+            })
+        },
+		_addComment: function(e) {
+			e.preventDefault();
+			// debugger;
+			console.log(this.props.collection)
+			var msg = {
+				message: this.refs.newMessage.getDOMNode().value
+			}
+			// var adding = new Backbone.CommentModel(msg)
+			this.props.collection.create(msg)
+			var form = document.querySelector('.addComment');
+			form.reset();
+		},
+		render: function() {
+			var comments = this.props.collection.models;
+			return z('div.comments-wrapper', [
+				z(Backbone.HeaderComponent),
+				z('form.addComment', {onSubmit: this._addComment}, [
+					z('input:text@newMessage'),
+					z('button', 'submit!')
+				]),
+				z('div.allComments',
+					comments.map(function(d, i, a) {
+						console.log(d);
+						return z(Backbone.CommentComponent, {key: i, d: d})
+					})
+				)
+			])
+		}
+	});
 	
 	
 })(typeof module === 'object' ? module.exports: window);
